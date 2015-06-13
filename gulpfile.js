41d9dfdd44d3a9c46870a8b13ef24cb679df426c
benweet <benoit.schweblin@gmail.com>
2015-06-13 13:31:44 +0100

    Use gulp-connect

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,25 @@
 var gulp = require('gulp');
-var express = require('express');
-var serveStatic = require('serve-static');
+var connect = require('gulp-connect');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
-gulp.task('express', function() {
-	var app = express();
-	app.use(serveStatic(__dirname));
-	var port = process.env.PORT || 11583;
-	app.listen(port, null, function() {
-		console.log('Server started: http://localhost:' + port);
-	});
+gulp.task('connect', function() {
+    connect.server({
+        port: 8888
+    });
 });
 
-gulp.task('run', [
-	'express'
-]);
-
 gulp.task('build-js', function() {
-	gulp.src('scripts/*.js')
-	.pipe(concat('cledit.js'))
-	.pipe(gulp.dest('dist'));
+    gulp.src('scripts/*.js')
+        .pipe(concat('cledit.js'))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build-js-min', function() {
-	gulp.src('scripts/*.js')
-	.pipe(concat('cledit-min.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest('dist'));
+    gulp.src('scripts/*.js')
+        .pipe(concat('cledit-min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', ['build-js', 'build-js-min']);
