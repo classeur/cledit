4385e72db83e6a7ac344fee5504f4413c54b31e0
benweet <benoit.schweblin@gmail.com>
2015-11-15 14:06:46 +0000

    Added highlighted event

--- scripts/cleditHighlighter.js
+++ scripts/cleditHighlighter.js
@@ -166,6 +166,7 @@
                     childNode = nextNode;
                 }
                 this.addTrailingNode();
+                self.$trigger('highlighted');
                 editor.selectionMgr.restoreSelection();
                 editor.selectionMgr.updateCursorCoordinates();
             }).cl_bind(this));
